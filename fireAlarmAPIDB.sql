6721e68f1b0dc342fd5954e46bb4934e4ddc88a0
Milinda Ranawaka <49589047+MilindaRanawaka@users.noreply.github.com>
2020-04-24 23:43:54 +0530

    Update DB

--- fireAlarmAPIDB.sql
+++ fireAlarmAPIDB.sql
@@ -13,9 +13,20 @@ create table sensors(
     primary key (sensorID)
 );
 
+create table users(
+	userID int NOT NULL AUTO_INCREMENT,
+	username varchar(20),
+	password varchar(20),
+    email varchar(20),
+	contactno varchar(20),
+    primary key (userID)
+);
+
 #Data
 insert into sensors(sensorStatus,sensorLocationFloorNo,sensorLocationRoomNo,smokeLevel,co2Level) values(true,"F5","N502",2,3);
 insert into sensors(sensorStatus,sensorLocationFloorNo,sensorLocationRoomNo,smokeLevel,co2Level) values(false,"F3","N302",1,1);
 
+insert into users(username,password,email,contactno) values('admin','admin','userAdmin@gmail.com','+94718956912');
 #Verify
 select * from sensors;
+select * from users;
